041e01a4023057acc6c94f24e72b88c61a9237ca
daniele_florean <daniele.florean@ituna.it>
2022-11-15 14:05:53 +0100

    added navigate to avoid first component load problem

---
+++ Daniflorex.StaticSpaHost.Shared/CmsData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daniflorex.StaticSpaHost.Shared
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Fields
+    {
+        public string title { get; set; }
+        public string text { get; set; }
+    }
+
+    public class Root
+    {
+        public System system { get; set; }
+        public Fields fields { get; set; }
+    }
+
+    public class System
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string urlSegment { get; set; }
+        public string type { get; set; }
+        public DateTime createdAt { get; set; }
+        public DateTime editedAt { get; set; }
+        public string contentType { get; set; }
+        public string locale { get; set; }
+        public object url { get; set; }
+    }
+}
