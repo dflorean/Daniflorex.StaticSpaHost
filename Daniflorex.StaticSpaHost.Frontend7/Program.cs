870a139b312b8f672598b21012002e898c306ca8
daniele_florean <daniele.florean@ituna.it>
2022-11-15 16:17:43 +0100

    added alternative .NET 7 frontend project

---
+++ Daniflorex.StaticSpaHost.Frontend7/Program.cs
@@ -0,0 +1,22 @@
+using Daniflorex.StaticSpaHost.Frontend7;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+//mod to lock prerendering
+if (!builder.RootComponents.Any())
+{
+    builder.RootComponents.Add<App>("#app");
+    builder.RootComponents.Add<HeadOutlet>("head::after");
+}
+
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+ConfigureServices(builder.Services, builder.HostEnvironment.BaseAddress);
+
+await builder.Build().RunAsync();
+
+static void ConfigureServices(IServiceCollection services, string baseAddress)
+{
+    services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
+    //services.AddScoped<IFoo, MyFoo>();
+}
